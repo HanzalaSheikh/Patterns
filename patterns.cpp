34e6f756b5f3769b1486ef138113a4e6def0c917
Hanzala <muhammadhanzala214@gmail.com>
2023-05-09 11:20:32 +0500

    2 Pattern programs Added

---
+++ patterns.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    // Patterns 1 2 3 4
+
+    // // 1)
+    // int count;
+    // cout<< "Enter last row number: ";
+    // cin >> count;
+    // for (int i = 1; i <= count; i++)
+    // {
+    //     for (int j = 1; j <= count; j++)
+    //     {
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+    
+    // Pattern 3 2 1
+
+    // 2)
+    // int count;
+    // cout<< "Enter last row number: ";
+    // cin >> count;
+    // for (int i = 1; i <= count; i++)
+    // {
+    //     for (int j = count; j!= 0; j--)
+    //     {
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
+    
+}
